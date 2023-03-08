3aeb3d110863aa64d05328d73ebf7871dc61e734
Kerdevez Tanguy <tkerdevez001@iutbayonne.univ-pau.fr>
2023-03-08 11:21:36 +0100

    Update AvisCoursController.php

--- edt/src/Controller/Api/AvisCoursController.php
+++ edt/src/Controller/Api/AvisCoursController.php
@@ -70,4 +70,52 @@ class AvisCoursController extends AbstractController
 
         return $this->json($avis, Response::HTTP_CREATED);
     }
+
+    #[Route('/{id}/avis', name: 'delete', methods: ['DELETE'])]
+    public function delete(AvisCoursRepository $repository, ?AvisCours $avis): JsonResponse
+    {
+        if (is_null($avis)) {
+            return $this->json([
+                'message' => 'Cet avis est introuvable',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $repository->remove($avis, true);
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/{id}', name: 'edit', methods: ['PATCH'])]
+    public function edit(Request $request, ?AvisCours $avis, AvisCoursRepository $repository, ValidatorInterface $validator): JsonResponse
+    {
+        if (is_null($avis)) {
+            return $this->json([
+                'message' => 'Cet avis est introuvable',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if (is_null($data)) {
+            return $this->json([
+                'message' => 'Requête mal formattée',
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $avis->fromArray($data);
+
+        $errors = $validator->validate($avis);
+
+        if ($errors->count() > 0) {
+            $messages = [];
+            foreach ($errors as $error) {
+                $messages[$error->getPropertyPath()] = $error->getMessage();
+            }
+            return $this->json($messages, Response::HTTP_BAD_REQUEST);
+        }
+
+        $repository->save($avis, true);
+
+        return $this->json($avis, Response::HTTP_CREATED);
+    }
 }
